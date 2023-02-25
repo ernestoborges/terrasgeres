a731989d4aa80476761acf6f3672dabf9c618fb9
Ernesto Neto <ernestoborges1995@gmail.com>
2023-02-25 18:25:54 -0300

    feat: basic moviment animation

--- src/components/canvas/Canvas.tsx
+++ src/components/canvas/Canvas.tsx
@@ -33,36 +33,95 @@ export function Canvas() {
                 const image = new Image();
                 image.src = "/src/assets/maps/village/background.png";
 
-
                 // player
                 const playerImage = new Image();
                 playerImage.src = "/src/assets/characters/human/male/walk-front.png";
 
+                // SpriteProps
 
-                image.onload = () => {
-                    context.drawImage(image, -startPosition.x, -startPosition.y);
-                    context.drawImage(
+                interface SpriteProps {
+                    position: {
+                        x: number, 
+                        y: number
+                    };
+                    image: HTMLImageElement
+                }
+
+                class Sprite  {
+                    position: { x: number; y: number; };
+                    image: HTMLImageElement;
+                   
+                    constructor({position, image}: SpriteProps){
+                        this.position = position;
+                        this.image = image;
+                    }
+
+                    draw(){
+                        context?.drawImage(this.image, this.position.x, this.position.y);
+                    }
+                }
+
+                const background = new Sprite({position: {x: -startPosition.x, y: -startPosition.y}, image: image});
+               
+                // animation
+
+                function animate(){
+                    window.requestAnimationFrame(animate)
+
+                    // background
+                    background.draw();
+                    // player
+                    context?.drawImage(
                         playerImage,
                         0,
                         0,
                         playerImage.width / 4,
                         playerImage.height,
-                        canvaSize.width / 2 - ( playerImage.width / 4 ) / 2,
+                        canvaSize.width / 2 - (playerImage.width / 4) / 2,
                         canvaSize.height / 2 - playerImage.height / 4,
                         playerImage.width / 4,
                         playerImage.height
                     );
-
                 }
-            }
+                animate();
+ 
+                // move player
+
+                const handleCanvasKeydown = (event: KeyboardEvent) => {
+                    switch (event.key) {
+                        case "w":
+                            console.log(event.key);
+                            background.position.y += 16 * 4;
+                            break;
+                        case "a":
+                            console.log(event.key);
+                            background.position.x += 16 * 4;
+                            break;
+                        case "s":
+                            console.log(event.key);
+                            background.position.y -= 16 * 4;
+                            break;
+                        case "d":
+                            console.log(event.key);
+                            background.position.x -= 16 * 4;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+
+                canvas.onkeydown = handleCanvasKeydown
+
         }
+    }
 
     }, []);
 
-
     return (
-        <canvas ref={canvasRef} className="main-canvas">
-
-        </canvas>
+        <canvas
+            ref={canvasRef}
+            className="main-canvas"
+            tabIndex={0}
+        />
     )
 }
