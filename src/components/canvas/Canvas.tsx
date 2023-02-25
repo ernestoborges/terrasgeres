f561083588b6fbc71e38bab856040d3a0f675e76
Ernesto Neto <ernestoborges1995@gmail.com>
2023-02-25 17:00:57 -0300

    feat: player creation

--- src/components/canvas/Canvas.tsx
+++ src/components/canvas/Canvas.tsx
@@ -5,19 +5,55 @@ export function Canvas() {
 
     const canvasRef = useRef<HTMLCanvasElement>(null);
 
+    const canvaSize = {
+        width: 1024,
+        height: 576
+    }
+
+    const startPosition = {
+        x: canvaSize.width / 2,
+        y: canvaSize.height / 2
+    }
+
 
     useEffect(() => {
         const canvas = canvasRef.current;
-        
+
         if (canvas) {
-            canvas.width = 1024;
-            canvas.height= 576;
-            
+            canvas.width = canvaSize.width;
+            canvas.height = canvaSize.height;
+
             const context = canvas.getContext("2d");
             // draw
             if (context) {
                 context.fillStyle = '#FF0000'
                 context.fillRect(0, 0, context.canvas.width, context.canvas.height)
+
+                // background
+                const image = new Image();
+                image.src = "/src/assets/maps/village/background.png";
+
+
+                // player
+                const playerImage = new Image();
+                playerImage.src = "/src/assets/characters/human/male/walk-front.png";
+
+
+                image.onload = () => {
+                    context.drawImage(image, -startPosition.x, -startPosition.y);
+                    context.drawImage(
+                        playerImage,
+                        0,
+                        0,
+                        playerImage.width / 4,
+                        playerImage.height,
+                        canvaSize.width / 2 - ( playerImage.width / 4 ) / 2,
+                        canvaSize.height / 2 - playerImage.height / 4,
+                        playerImage.width / 4,
+                        playerImage.height
+                    );
+
+                }
             }
         }
 
